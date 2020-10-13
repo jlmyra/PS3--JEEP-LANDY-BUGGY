03ae9450995b57acf394c2083fdd8c0c6f59ff4b
jlmyra <jlmyra@gmail.com>
2020-10-13 16:29:59 -0400

    Add files via upload

--- Citations.ino
+++ Citations.ino
@@ -7,7 +7,7 @@
  I had to make a modification to this library so that I could incorporate both servos and motors in my sketch.
  ESP32Servo.h - changed line 96 espservo.h to 2 servos max leaving channels 2-16 availble for motors
 
-  And thanks to these sites for sharing information on coding the ESP32 with PS3, PWM motors and servos.
+ And thanks to these sites for sharing information on coding the ESP32 with PS3, PWM motors and servos.
 https://dronebotworkshop.com/esp32-servo/
 
 https://techtutorialsx.com/2020/03/28/esp32-ps3-controller-controlling-a-dc-motor/
@@ -21,6 +21,32 @@ and to:
 RENZO MISCHIANTI for his write-up on voltage dividers
 https://www.mischianti.org/2019/06/15/voltage-divider-calculator-and-application/
 
+
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
 /*
  
 Ps3.event.button_down.____
