abc5fed1d9a34d941200a9f5b334f4206abc9097
jlmyra <jlmyra@gmail.com>
2020-10-13 16:25:54 -0400

    Update Citations.ino

--- Citations.ino
+++ Citations.ino
@@ -9,5 +9,28 @@ https://randomnerdtutorials.com/esp32-dc-motor-l298n-motor-driver-control-speed-
 
 https://techtutorialsx.com/2020/03/28/esp32-ps3-controller-controlling-a-dc-motor/?fbclid=IwAR1NgVzkMwT703TBaM5_dPxJSR-Dr_jiYZeYUreNiyxF4XkeAotPT4RZdlY
 
+This is free and unencumbered software released into the public domain.
 
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
 */
